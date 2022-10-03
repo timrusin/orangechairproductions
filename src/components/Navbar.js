93a22ff1edd7adde1483eca4e204d4cb5fce8dd2
timrusin <timrusin@gmail.com>
2022-10-02 20:19:31 -0500

    applied home link to img as well as saturation boost on hover

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,11 +18,12 @@ const Navbar = () => {
         </div>
 
         <div className='title'>
+        <Link to="/">
           <img 
           src="/img/cropped-orangechair-cleaned-edge-logo-2020-3-1.png" 
           alt="orange chair productions logo" 
           className='logo'/>
-          <Link to="/"><h1 className="main-title">Orange Chair Productions</h1></Link>
+          <h1 className="main-title">Orange Chair Productions</h1></Link>
           <h4 className="sub-title">A Boutique Production Company Offering A La Carte or Complete Service for the Film & Video World</h4>
         </div>
 
