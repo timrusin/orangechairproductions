4366fdb821f67b2336bd318355b530d8f9ae3ea9
timrusin <timrusin@gmail.com>
2022-10-07 00:12:44 -0500

    removed unused stuff in nav component that was throwing off netlify

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,10 @@
-import { useState } from 'react'
 import "./Navbar.css"
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  const [click, setClick] = useState(false)
-  const handleClick = () => setClick (!click)
-  const closeMenu = () => setClick (false)
 
   return (
-
     <nav className='navbar'>
-      {/* <div className={click ? "navbar active" : "navbar"}>
-        <div className="menu-icon" onClick={handleClick}>
-          <i className={click ? "fas fa-dash menu-dashes-angle1" : "fas fa-dash menu-dashes"}/>
-          <i className={click ? "fas fa-dash menu-dashes-angle2" : "fas fa-dash menu-dashes"}/>
-          <i className={click ? "fas fa-dash menu-dashes-angle2" : "fas fa-dash menu-dashes"}/>
-        </div> */}
-
         <div className='title'>
           <Link className="title-link" to="/">
             <img
@@ -27,17 +15,6 @@ const Navbar = () => {
             <h4 className="sub-title">A Boutique Production Company Offering A La Carte or Complete Service for the Film & Video World</h4>
           </Link>
         </div>
-
-        {/* <div className={click ? "nav-menu active" : "nav-menu"}>
-          <nav className="nav-links">
-            <Link to="" className='nav-item' onClick={closeMenu}>Link 1</Link>
-            <Link to="" className='nav-item' onClick={closeMenu}>Link 2</Link>
-            <Link to="" className='nav-item' onClick={closeMenu}>Link 3</Link>
-            
-          </nav>
-        </div> */}
-      {/* </div> */}
-
     </nav>
   );
 }
